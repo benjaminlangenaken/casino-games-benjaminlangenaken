aee853ba1b5e73722a1af55d670d5a50f6906f65
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-01-20 11:18:18 +0100

    Optimize styling

--- 01-rock-paper-scissors/script.js
+++ 01-rock-paper-scissors/script.js
@@ -1,55 +1,96 @@
 // Click on play button:
-function clickOnPlay(event) {
-    const getH2 = document.querySelector("h2");
-    getH2.style.visibility = "visible";
+const buttons = document.querySelectorAll('.btn');
 
-    const getIcons = document.querySelector(".icons")
-    getIcons.style.visibility = "visible";
-}
-
-const buttons = document.querySelectorAll('.btn')
 for (let button of buttons) {
-    button.addEventListener('click', clickOnPlay)
+    button.addEventListener('click', clickOnPlay = (event) => {
+        const getH2 = document.querySelector("h2");
+        getH2.style.visibility = "visible";
+
+        const getIcons = document.querySelector(".icons");
+        getIcons.style.visibility = "visible";
+
+    }, { once: true })
 }
 
 // Create random computer choice:
-const compIcon = ["fas fa-hand-rock fa-7x", "fas fa-hand-paper fa-7x", "fas fa-hand-scissors fa-7x"]
+const icons = document.querySelectorAll('.fas');
+
 randomIcon = () => {
     const random = Math.floor(Math.random() * 3);
-    console.log(compIcon[random])
+    return (icons[random].className)
 };
 
 // Click on icons:
-function clickOnIcon(event) {
-
-    const addA = document.createElement("a");
-    const addI = document.createElement("i");
-
-    // Add players choice:
-    const yourChoice = document.querySelector(".your-choice");
-    yourChoice.style.visibility = "visible";
-
-    const choice = document.querySelector(".playersChoice");
-    choice.append(addA);
-    addA.appendChild(addI);
-    addI.className = `fas ${event.target.classList[1]} fa-7x`
-
-    // Add computers choice:
-    // const computersChoice = document.querySelector(".computers-choice");
-    // computersChoice.style.visibility = "visible";
-
-    // const compChoice = document.querySelector(".playersChoice");
-    // compChoice.append(addA);
-    // addA.appendChild(addI);
-    // addI.className = `${randomIcon(event)}`;
-
-    console.log(addI.className)
-}
-
-const icons = document.querySelectorAll('.fas')
 for (let icon of icons) {
-    icon.addEventListener('click', clickOnIcon)
+    icon.addEventListener('click', clickOnIcon = (event) => {
+
+        const addA = document.createElement("a");
+        const addI = document.createElement("i");
+
+        // Add players choice:
+        const yourChoice = document.querySelector(".your-choice");
+        yourChoice.style.visibility = "visible";
+
+        const choice = document.querySelector(".playersChoice");
+        choice.append(addA);
+        addA.appendChild(addI);
+        addI.className = `fas ${event.target.classList[1]} fa-7x`;
+
+        // Make sure click event only happens once
+    }, { once: true })
 }
 
 
-// Confirm player's choice:
+// Add computers choice:
+
+for (let icon of icons) {
+    icon.addEventListener('click', clickOnIcon = (event) => {
+
+        const addA = document.createElement("a");
+        const addI = document.createElement("i");
+
+        // Add computers choice:
+        const computersChoice = document.querySelector(".computers-choice");
+        computersChoice.style.visibility = "visible";
+
+        const compChoice = document.querySelector(".computersChoice");
+        compChoice.append(addA);
+        addA.appendChild(addI);
+        addI.className = `${randomIcon(event)}`;
+
+        // Make sure click event only happens once
+    }, { once: true })
+}
+
+
+// Show result:
+for (let icon of icons) {
+    icon.addEventListener('click', clickOnIcon = (event) => {
+
+        // const result = document.querySelector(".winner");
+        // result.style.visibility = "visible";
+
+        const playerResult = document.querySelector(".playersChoice i").className.slice(12, length - 6);
+        const compResult = document.querySelector(".computersChoice i").className.slice(12, length - 6)
+
+        console.log(playerResult);
+        console.log(compResult);
+
+        if (
+            playerResult === "rock" && compResult === "rock" ||
+            playerResult === "paper" && compResult === "paper" ||
+            playerResult === "scissors" && compResult === "scissors"
+        ) {
+            const winnerTxt = document.querySelector(".winnerText")
+            const addH1 = document.createElement("h1");
+            addH1.className = "logo lg";
+            addH1.innerHTML = "It's a draw... &#128528;"
+            winnerTxt.append(addH1)
+        } // else if {
+        //     playerResult === "rock" && compResult === "rock" ||
+        //     playerResult === "paper" && compResult === "paper" ||
+        //     playerResult === "scissors" && compResult === "scissors"
+        // }
+
+    }, { once: true })
+}
